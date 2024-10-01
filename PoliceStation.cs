0f64e1781d2f9178d57e4b86a1ed18bf79edd8f8
Gonzalo Borrachero <202215485@alu.comillas.edu>
2024-10-01 19:01:01 +0200

    Creo la clase comisaría con sus atributos y métodos

---
+++ PoliceStation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Practice1
+{
+	public class PoliceStation
+	{
+		private int number; // Added to difference between Police Stations
+		public List<PoliceCar> registeredCars { get; private set; }
+
+        public PoliceStation(int number)
+		{
+			this.number = number;
+            registeredCars = new List<PoliceCar>();
+        }
+
+		public void RegisterCar(PoliceCar policeCar)
+		{
+			registeredCars.Add(policeCar);
+		}
+		public void ActivateAlarm(Vehicle vehicle)
+		{
+			for(int i=0; i < registeredCars.Count; i++)
+			{
+				PoliceCar car = registeredCars[i];
+				car.SetChasingVehicle(vehicle);
+			}
+		}
+	}
+}
+
