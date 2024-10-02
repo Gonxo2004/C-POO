e924a0253df195470c67494c1872445c108fbdf4
Gonzalo Borrachero <202215485@alu.comillas.edu>
2024-10-02 10:07:28 +0200

    Creo dos clases hijas de vehículo(con y sin matrícula) para no romper el Liskov Substitution Principle

---
+++ VehicleWithPlate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Practice1
+{
+	public abstract class VehicleWithPlate : Vehicle, IMessageWritter
+	{
+        private string plate;
+
+        public VehicleWithPlate(string typeOfVehicle, string plate) : base(typeOfVehicle)
+		{
+            this.plate = plate;
+        }
+
+        public string GetPlate()
+        {
+            return plate;
+        }
+        //Override ToString() method with class information
+        public override string ToString()
+        {
+            return $"{GetTypeOfVehicle()} with plate {GetPlate()}";
+        }
+
+        //Implment interface with Vechicle message structure
+        public string WriteMessage(string message)
+        {
+            return $"{this}: {message}";
+        }
+    }
+}
+
---
+++ VehicleWithoutPlate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Practice1
+{
+	public abstract class VehicleWithoutPlate:Vehicle, IMessageWritter
+	{
+		public VehicleWithoutPlate(string typeOfVehicle) : base(typeOfVehicle)
+        {
+		}
+        //Override ToString() method with class information
+        public override string ToString()
+        {
+            return $"{GetTypeOfVehicle()}";
+        }
+
+        //Implment interface with Vechicle message structure
+        public string WriteMessage(string message)
+        {
+            return $"{this}: {message}";
+        }
+    }
+}
+
