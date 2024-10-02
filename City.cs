7c8254d1a77aa1aff4e4906061fedaa861071d18
Gonzalo Borrachero <202215485@alu.comillas.edu>
2024-10-02 12:11:02 +0200

    Creo la clase City y Scooter (hija de VehicleWithoutPlate

---
+++ City.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Practice1
+{
+	public class City: IMessageWritter
+	{
+        public List<Taxi> registeredTaxis { get; private set; }
+        //private PoliceStation policeStation;
+
+        public City()
+		{
+            registeredTaxis = new List<Taxi>();
+            Console.WriteLine(WriteMessage("Created"));
+        }
+
+        public void AddTaxiLicense(string plate)
+        {
+            Taxi taxi = new Taxi(plate);
+            registeredTaxis.Add(taxi);
+            Console.WriteLine(WriteMessage($"Added license of Taxi with plate {plate}"));
+        }
+
+        public void RemoveTaxiLicense(string plate)
+        {
+            for (int i = 0; i < registeredTaxis.Count; i++)
+            {
+                Taxi taxi = registeredTaxis[i];
+                if (taxi.GetPlate() == plate)
+                {
+                    registeredTaxis.Remove(taxi);
+                    Console.WriteLine(WriteMessage($"Removed license of Taxi with plate {plate}"));
+                }
+            }
+
+        }
+        //Override ToString() method with class information
+        public override string ToString()
+        {
+            return $"City";
+        }
+
+        //Implment interface with Vechicle message structure
+        public string WriteMessage(string message)
+        {
+            return $"{this}: {message}";
+        }
+    }
+}
+
---
+++ Scooter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Practice1
+{
+	public class Scooter: VehicleWithoutPlate
+	{
+        private static string typeOfVehicle = "Scooter";
+        public Scooter() : base(typeOfVehicle)
+		{
+		}
+
+
+	}
+}
+
